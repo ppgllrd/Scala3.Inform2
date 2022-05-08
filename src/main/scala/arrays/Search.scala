6fe3f3df050f58971b2d5eaac7a86e80fb6f696c
ppgllrd <ppgllrd@gmail.com>
2022-05-08 23:17:22 +0200

    Add selection sort, linear search and binary search.

---
+++ src/main/scala/arrays/Search.scala
@@ -0,0 +1,75 @@
+/*
+  Pepe Gallardo, 2022
+ */
+
+package arrays
+
+object Search:
+
+  // Left to right linear search of element x in array xs.
+  // Returns Some(index) if x was firstly found at xs(index)
+  // or None if x is not in xs
+  def linearSearch[A](x: A, xs: Array[A]): Option[Int] =
+    var opt: Option[Int] = None // element not found yet
+    var i = 0 // we start at index 0 of array
+
+    // while not found and while i is a valid index
+    while opt.isEmpty && i < xs.length do
+      if xs(i) == x then
+        opt = Some(i) // element found at index i. Stops loop
+      else
+        i += 1 // not found at index i. Move to next cell in array
+
+    opt
+
+
+  def binarySearch[A](x: A, xs: Array[A])(using ord: Ordering[A]): Option[Int] =
+    import ord._
+    // precondition: xs must be sorted in ascending order
+
+    var opt: Option[Int] = None
+    var left = 0
+    var right = xs.length - 1
+
+    while opt.isEmpty && left <= right do
+      val mid = (left + right) / 2
+      val y = xs(mid)
+      if x == y then
+        opt = Some(mid)
+      else if x > y then
+        left = mid + 1
+      else
+        right = mid - 1
+
+    opt
+
+
+@main def searchTest(): Unit =
+  def printResult(opt: Option[Int]): Unit =
+    println(opt match
+              case Some(index) => s"Element was found at cell $index"
+              case None => s"Element was not found"
+    )
+
+  val xs = Array(7,4,2,0,5,3,1,4)
+
+  val opt1 = Search.linearSearch(0, xs)
+  printResult(opt1)
+
+  val opt2 = Search.linearSearch(4, xs)
+  printResult(opt2)
+
+  val opt3 = Search.linearSearch(9, xs)
+  printResult(opt3)
+
+  // Array must be sorted for using binary Search
+  val ys = Array(0,1,2,3,4,5,7,8)
+
+  val opt4 = Search.binarySearch(0, ys)
+  printResult(opt4)
+
+  val opt5 = Search.binarySearch(4, ys)
+  printResult(opt5)
+
+  val opt6 = Search.binarySearch(9, ys)
+  printResult(opt6)
---
+++ src/main/scala/arrays/Sort.scala
@@ -0,0 +1,130 @@
+/*
+  Pepe Gallardo, 2022
+ */
+
+package arrays
+
+object Sort:
+  // checks whether array xs is sorted in ascending order
+  def isSorted[A](xs: Array[A])(using ord: Ordering[A]): Boolean =
+    import ord._
+
+    var sorted = true
+    var i = 0
+    while sorted && i < xs.length - 1 do
+      if xs(i) <= xs(i + 1) then
+        i += 1
+      else
+        sorted = false
+    sorted
+
+  // In place sorts an array using Selection Sort algorithm
+  def selectionSort[A](xs: Array[A])(using ord: Ordering[A]): Unit =
+    import ord._
+
+    // Swaps elements in array at indexes i and j
+    def swap(i: Int, j: Int): Unit =
+      val temp = xs(i)
+      xs(i) = xs(j)
+      xs(j) = temp
+
+    for i <- 0 until xs.length - 1 do
+      var minIndex = i
+      for j <- i + 1 until xs.length do
+        if xs(j) < xs(minIndex) then
+          minIndex = j
+      swap(i, minIndex)
+
+
+  // In place sorts an array using Quick Sort algorithm
+  def quickSort[A](xs: Array[A])(using ord: Ordering[A]): Unit =
+    import ord._
+
+    // Swaps elements in array at indexes i and j
+    def swap(i: Int, j: Int): Unit =
+      val temp = xs(i)
+      xs(i) = xs(j)
+      xs(j) = temp
+
+    // Partitions elements in fragment of array going from xs(left) to xs(right).
+    // Returns final position of pivot.
+    // All elements to the left of pivot will be smaller than pivot.
+    // All elements to the right of pivot will be larger than or equal to the pivot.
+    def partition(left: Int, right: Int): Int =
+      val mid = (left + right) / 2
+      val pivot = xs(mid)
+
+      swap(mid, right) // place pivot at the end
+
+      // Push elements smaller than the pivot to the left.
+      // Invariant: all elements at the left of index p are smaller
+      // than the pivot.
+      var p = left
+      for i <- left until right do
+        if xs(i) < pivot then
+          swap(p, i)
+          p += 1
+
+      swap(p, right) // finally, place pivot after smaller elements
+      p
+
+    // sorts fragment of array going from xs(left) to xs(right)
+    def aux(left: Int, right: Int): Unit =
+      if right > left then // if fragment has at least 2 elements
+        val pivotIndex = partition(left, right)
+        aux(left, pivotIndex - 1)
+        aux(pivotIndex + 1, right)
+
+    aux(0, xs.length - 1) // initial call to sort full array
+
+
+@main def quickSortTest(): Unit =
+  val xs = Array(6, 4, 3, 8, 1, 4, 9, 2, 5, 7)
+  Sort.quickSort(xs)
+  println(s"Sorted array is ${xs.mkString("Array(", ", ", ")")}")
+
+  val ys = Array("John", "Peter", "Arthur", "Mike")
+  Sort.quickSort(ys)
+  println(s"Sorted array is ${ys.mkString("Array(", ", ", ")")}")
+
+
+// checks whether arrays xs and ys have same elements (may be in different orders)
+def sameElements[A](xs: Array[A], ys: Array[A]): Boolean =
+  xs.diff(ys).isEmpty && ys.diff(xs).isEmpty
+
+
+def SortCorrectness(name: String, sort: Array[Int] => Unit): Unit =
+  val numTests = 1000 // number of tests to perform
+  val length = 100 // length of each random array used for testing
+
+  for seed <- 0 until numTests do
+    // create an array of random integers
+    val rnd = scala.util.Random(seed)
+    val xs = Array.fill(length)(rnd.nextInt())
+
+    // make a copy of the array
+    val ys = xs.clone()
+
+    // sort the copy
+    sort(ys)
+
+    // the sorted array must be in ascending order and
+    // original array and sorted one should have same
+    // elements (although they could be in different orders)
+    val correct = Sort.isSorted(ys) && sameElements(xs, ys)
+
+    if !correct then
+      sys.error(s"$name algorithm failed")
+
+  // if we reach this point without an error, then we passed all tests
+  println(s"$name algorithm passed all sorting tests")
+
+
+// tests Selection Sort with a different random arrays
+@main def selectionSortCorrectness(): Unit =
+  SortCorrectness("Selection Sort", Sort.selectionSort)
+
+
+// tests Quick Sort with different random arrays
+@main def quickSortCorrectness(): Unit =
+  SortCorrectness("Quick Sort", Sort.quickSort)
