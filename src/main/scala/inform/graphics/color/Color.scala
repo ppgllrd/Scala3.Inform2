ae593cfe89da2bee6f6f68d6fb27fb31f724aa7e
ppgllrd <ppgllrd@gmail.com>
2022-05-16 00:53:15 +0200

    Colors.

---
+++ src/main/scala/inform/graphics/color/Color.scala
@@ -0,0 +1,120 @@
+/*
+  Pepe Gallardo, 2022
+ */
+
+package inform.graphics.color
+
+object Color:
+  val red: Color = new Color(255, 0, 0)
+  val blue: Color = new Color(0, 0, 255)
+  val green: Color = new Color(0, 255, 0)
+  val yellow: Color = new Color(255, 255, 0)
+  val cyan: Color = new Color(0, 255, 255)
+  val magenta: Color = new Color(255, 0, 255)
+  val pink: Color = new Color(255, 175, 175)
+  val orange: Color = new Color(255, 200, 0)
+  val white: Color = new Color(255, 255, 255)
+  val black: Color = new Color(0, 0, 0)
+  val gray: Color = new Color(128, 128, 128)
+
+  /**
+   * @return New color from its (Red, Green, Blue) components
+   *         <a href="https://en.wikipedia.org/wiki/RGB_color_model">RGB</a>.
+   *         Each parameter must be in range [0,255].
+   */
+  def apply(red: Int, green: Int, blue: Int): Color =
+    new Color(red, green, blue)
+    
+
+  /**
+   * @return New color from its (Red, Green, Blue, Alpha) components
+   *         <a href="https://en.wikipedia.org/wiki/RGBA_color_model">RGBA</a>.
+   *         Each parameter must be in range [0,255].
+   */
+  def apply(red: Int, green: Int, blue: Int, alpha: Int): Color =
+    new Color(red, green, blue, alpha)
+
+  
+  /**
+   * @return New color from its (Red, Green, Blue, Alpha) components
+   *         <a href="https://en.wikipedia.org/wiki/RGBA_color_model">RGBA</a>.
+   *         Each parameter must be in range [0,255].
+   */
+  def rgba(red: Int, green: Int, blue: Int, alpha: Int): Color =
+    new Color(red, green, blue, alpha)
+
+  
+  /**
+   * @return New color from its (Red, Green, Blue) components
+   *         <a href="https://en.wikipedia.org/wiki/RGB_color_model">RGB</a>.
+   *         Each parameter must be in range [0,255].
+   */
+  def rgb(red: Int, green: Int, blue: Int): Color =
+    new Color(red, green, blue)
+
+
+  /**
+   * @return Deconstructs a color into its (Red, Green, Blue) components
+   *         <a href="https://en.wikipedia.org/wiki/RGB_color_model">RGB</a>.
+   */
+  def unapply(c: Color): Option[(Int, Int, Int)] =
+    Some(c.getRed, c.getGreen, c.getBlue)
+
+  
+  object RGB:
+    /**
+     * @return New color from its (Red, Green, Blue) components
+     *         <a href="https://en.wikipedia.org/wiki/RGB_color_model">RGB</a>.
+     *         Each parameter must be in range [0,255].
+     */
+    def apply(red: Int, green: Int, blue: Int): Color =
+      new Color(red, green, blue)
+
+
+    /**
+     * @return Deconstructs a color into its (Red, Green, Blue) components
+     *         <a href="https://en.wikipedia.org/wiki/RGB_color_model">RGB</a>.
+     */
+    def unapply(c: Color): Option[(Int, Int, Int)] =
+      Some(c.getRed, c.getGreen, c.getBlue)
+  
+
+  object RGBA:
+    /**
+     * @return New color from its (Red, Green, Blue, Alpha) components
+     *         <a href="https://en.wikipedia.org/wiki/RGBA_color_model">RGBA</a>.
+     *         Each parameter must be in range [0,255].
+     */
+    def apply(red: Int, green: Int, blue: Int, alpha:Int): Color =
+        new Color(red, green, blue, alpha)
+
+    /**
+     * @return Deconstructs a color into its (Red, Green, Blue, Alpha) components
+     *         <a href="https://en.wikipedia.org/wiki/RGBA_color_model">RGBA</a>.
+     */
+    def unapply(c: Color): Option[(Int, Int, Int, Int)] =
+      Some(c.getRed, c.getGreen, c.getBlue, c.getAlpha)
+  
+  
+
+class Color(rgba: Int) extends java.awt.Color(rgba, true):
+  /**
+   * @return New color from its (Red, Green, Blue, Alpha) components
+   *         <a href="https://en.wikipedia.org/wiki/RGBA_color_model">RGBA</a>.
+   *         Each parameter must be in range [0,255].
+   */
+  def this(red: Int, green: Int, blue: Int, alpha: Int) =
+    this(((alpha & 0xFF) << 24) | ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | ((blue & 0xFF) << 0))
+
+  
+  /**
+   * @return New color from its (Red, Green, Blue) components
+   *         <a href="https://en.wikipedia.org/wiki/RGB_color_model">RGB</a>.
+   *         Each parameter must be in range [0,255].
+   */
+  def this(red: Int, green: Int, blue: Int) = 
+    this(red, green, blue, 255)
+  
+  
+  override def toString: String =
+    s"Color($getRed, $getGreen, $getBlue, $getAlpha)"
--- src/main/scala/photos/Photos.scala
+++ src/main/scala/photos/Photos.scala
@@ -8,29 +8,29 @@ import inform.fotos.*
 import inform.graphics.color.*
 
 object Photos:
-  val red: Color = Color(255,0,0)
-  val green: Color = Color(0,255,0)
-  val blue: Color = Color(0,0,255)
+  val red: Color = Color.RGB(255, 0, 0)
+  val green: Color = Color.RGB(0, 255, 0)
+  val blue: Color = Color.RGB(0, 0, 255)
 
-  val darkRed: Color = Color(128,0,0)
+  val darkRed: Color = Color.RGB(128, 0, 0)
 
-  val purple: Color = Color(255,0,255)
+  val purple: Color = Color.RGB(255,0,255)
 
-  val black: Color = Color(0,0,0)
-  val white: Color = Color(255,255,255)
-  val grays: Array[Color] = (for(x <- 0 to 255 by 32) yield new Color(x,x,x)).toArray
-
+  val black: Color = Color.RGB(0,0,0)
+  val white: Color = Color.RGB(255,255,255)
+  val grays: Array[Color] = (for(x <- 0 to 255 by 32) yield Color.RGB(x, x, x)).toArray
+  
   val r: Int = red.getRed
   val g: Int = red.getGreen
   val b: Int = red.getBlue
 
   def luminescence(color: Color): Double =
-    val Color(r, g, b, a) = color
+    val Color(r, g, b) = color
     0.299*r + 0.587*g + 0.114*b
 
   def toBlackWhite(color: Color): Color =
     val l = luminescence(color).toInt
-    Color(l,l,l)
+    Color.RGB(l, l, l)
 
   def toBlackWhite(photo: Foto): Foto =
     val height = photo.alto
@@ -53,7 +53,7 @@ object Photos:
 
   for r <- 0 until height do
     val v = r * 255 / height
-    val color = Color(v, v, v)
+    val color = Color.RGB(v, v, v)
     for c <- 0 until width do
       photo(r)(c) = color
 
@@ -61,19 +61,19 @@ object Photos:
 
 
 @main def photoTest2(): Unit =
-  import inform.util.aleatorios.*
+  import inform.util.random.*
 
   val height = 300
   val width = 200
   val photo = Foto(height,width)
 
-  val alt = Aleatorio(0)
+  val rnd = Random(0)
 
   for r <- 0 until height do
-    val red = alt.entero(0, 255)
-    val green = alt.entero(0, 255)
-    val blue = alt.entero(0, 255)
-    val color = Color(red, green, blue)
+    val red = rnd.uniform(256)
+    val green = rnd.uniform(256)
+    val blue = rnd.uniform(256)
+    val color = Color.RGB(red, green, blue)
 
     for c <- 0 until width do
       photo(r)(c) = color
